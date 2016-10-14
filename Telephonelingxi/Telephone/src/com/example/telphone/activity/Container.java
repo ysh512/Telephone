a30666a39f7616ae657a130e40d90a55b82da34e
shangyang <shang.yang@pptv.com>
2016-10-14 16:53:25 +0800

    share

--- Telephonelingxi/Telephone/src/com/example/telphone/activity/Container.java
+++ Telephonelingxi/Telephone/src/com/example/telphone/activity/Container.java
@@ -1185,6 +1185,7 @@ public class Container extends BaseActivity implements OnClickListener ,OnPageCh
 		 //分享网络图片，新浪微博分享网络图片需要通过审核后申请高级写入接口，否则请注释掉测试新浪微博
 //		 oks.setImageUrl("http://f1.sharesdk.cn/imgs/2014/02/26/owWpLZo_638x960.jpg");
 		 // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
+		 oks.setFilePath("file:///android_asset/logo.png");
 		 //oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
 		 // url仅在微信（包括好友和朋友圈）中使用
 		 oks.setUrl(shareUrl);
